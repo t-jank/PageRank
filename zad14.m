b1be8feae5160ad9b996894707466aa621f8a6ca
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-06-16 21:59:33 +0200

    plots fixed

--- zad14.m
+++ zad14.m
@@ -6,24 +6,27 @@ PG = [0   1/2 1/2  0   0;
       0   1/3  0  1/3 1/3;
       1    0   0   0   0;
       1/5 1/5 1/5 1/5 1/5;];
-colors = ['b' 'g' 'y' 'r' 'k' 'm' 'c']
+colors = ['b' "r" "#EDB120" "m" "#77AC30" "#4DBEEE", "#A2142F"];
 for a = 1:7
     MG = (1-alfa(a))*PG + alfa(a)*1/n*Jn;
     PI_i = zeros(1,n)+1/n;
-    %blad = sum(abs(PI_i-PI_i*MG)); %% 11n ODKOMENTOWAC MOZE JEDNAK %%
-    %plot(0,blad,'.','MarkerSize',20,'MarkerEdgeColor',colors(a)); hold on; grid on
+    %blad = sum(abs(PI_i-PI_i*MG));
+    %plot(0,blad,'.','MarkerSize',25,'MarkerEdgeColor',colors(a),'HandleVisibility','off'); hold on; grid on
     %disp("PI_0:")
     %disp(PI_i)
     for i=1:25
         PI_i = PI_i * MG;
-       % disp("PI_" + i + ":")
-      %  disp(PI_i)
+        %disp("PI_" + i + ":")
+        %disp(PI_i)
         blad = sum(abs(PI_i-PI_i*MG));
-        plot(i,blad,'.','MarkerSize',20,'MarkerEdgeColor',colors(a)); hold on; grid on
-        legend("alfa = " + alfa(a))
-        %title("alfa = " + alfa(a))
+        if mod(i,25) == 1
+            plot(i,blad,'.','MarkerSize',25,'MarkerEdgeColor',colors(a),'DisplayName',"\alpha = " + alfa(a)); hold on; grid on
+        else
+            plot(i,blad,'.','MarkerSize',25,'MarkerEdgeColor',colors(a),'HandleVisibility','off'); hold on; grid on
+        end
+        legend
+        title("Zbieganie do rozkładu stacjonarnego dla różnych wartości \alpha")
         xlabel('i')
-        ylabel('Błąd (|PI_i-PI_i*MG|)')
+        ylabel("Błąd (|\pi_i-\pi_i*M_G|)")
     end
 end
-%'MarkerEdgeColor','b'
